45c3e607006d8fc3672ff64adad780fd7884fa51
Guillermo Cruz <GuillermoCruz32>
2020-01-06 10:51:02 -0500

    Made some changes to the map example. Added coloring and node examples plus documentation.

--- static/js/code.js
+++ static/js/code.js
@@ -31,7 +31,7 @@ chart.projection = new am4maps.projections.Mercator();
 chart.zoomControl = new am4maps.ZoomControl();
 
 // Set initial zoom
-chart.homeZoomLevel = 2.5;
+chart.homeZoomLevel = 1.5;
 chart.homeGeoPoint = {
   latitude: 51,
   longitude: -23
@@ -45,32 +45,176 @@ polygonSeries.mapPolygons.template.nonScalingStroke = true;
 
 // Add images
 var imageSeries = chart.series.push(new am4maps.MapImageSeries());
+var imageSeries2 = chart.series.push(new am4maps.MapImageSeries());
+var imageSeries3 = chart.series.push(new am4maps.MapImageSeries());
+var imageSeries4 = chart.series.push(new am4maps.MapImageSeries());
+
 var imageTemplate = imageSeries.mapImages.template;
 imageTemplate.tooltipText = "{title}";
 imageTemplate.nonScaling = true;
+
+var imageTemplate2 = imageSeries2.mapImages.template;
+imageTemplate2.tooltipText = "{title}";
+imageTemplate2.nonScaling = true;
+
+var imageTemplate3 = imageSeries3.mapImages.template;
+imageTemplate3.tooltipText = "{title}";
+imageTemplate3.nonScaling = true;
+
+var imageTemplate4 = imageSeries4.mapImages.template;
+imageTemplate4.tooltipText = "{title}";
+imageTemplate4.nonScaling = true;
 
 var marker = imageTemplate.createChild(am4core.Sprite);
 marker.path = targetSVG;
 marker.horizontalCenter = "middle";
 marker.verticalCenter = "middle";
-marker.scale = 0.7;
-marker.fill = interfaceColors.getFor("alternativeBackground");
+marker.scale = 0.5;
+marker.fill = am4core.color("#0116FF");
+marker.stroke = am4core.color("#0116FF");
+
+var marker2 = imageTemplate2.createChild(am4core.Sprite);
+marker2.path = targetSVG;
+marker2.horizontalCenter = "middle";
+marker2.verticalCenter = "middle";
+marker2.scale = 0.5;
+marker2.fill = am4core.color("#1BFF00");
+marker2.stroke = am4core.color("#1BFF00");
+
+var marker3 = imageTemplate3.createChild(am4core.Sprite);
+marker3.path = targetSVG;
+marker3.horizontalCenter = "middle";
+marker3.verticalCenter = "middle";
+marker3.scale = 0.5;
+marker3.fill = am4core.color("#FFE000");
+marker3.stroke = am4core.color("#FFE000");
+
+var marker4 = imageTemplate4.createChild(am4core.Sprite);
+marker4.path = targetSVG;
+marker4.horizontalCenter = "middle";
+marker4.verticalCenter = "middle";
+marker4.scale = 0.5;
+marker4.fill = am4core.color("#000000");
+marker4.stroke = am4core.color("#000000");
+// marker.fill = interfaceColors.getFor("alternativeBackground");
 
 imageTemplate.propertyFields.latitude = "latitude";
 imageTemplate.propertyFields.longitude = "longitude";
 imageSeries.data = [ {
-  "id": "london",
   "svgPath": targetSVG,
-  "title": "London",
-  "latitude": 51.5002,
-  "longitude": -0.1262,
-  "scale": 1
+  "title": "Buenos Aires, Artist Name",
+  "latitude": -34.6037,
+  "longitude": -58.3816,
+  "scale": 0.5
+}, {
+  "svgPath": targetSVG,
+  "title": "Lima, Artist Name",
+  "latitude": -12.0464,
+  "longitude": -77.0428,
+  "scale": 0.5
+}, {
+  "svgPath": targetSVG,
+  "title": "Brasilia, Artist Name",
+  "latitude": -15.8267,
+  "longitude": -47.9218,
+  "scale": 0.5
+}, {
+  "svgPath": targetSVG,
+  "title": "Cape Town, Artist Name",
+  "latitude": -33.9249,
+  "longitude": 18.4241,
+  "scale": 0.5
+}, {
+  "svgPath": targetSVG,
+  "title": "Seoul, Artist Name",
+  "latitude": 37.5665,
+  "longitude": 126.9780,
+  "scale": 0.5
+}];
+
+imageTemplate3.propertyFields.latitude = "latitude";
+imageTemplate3.propertyFields.longitude = "longitude";
+imageSeries3.data = [ {
+  "svgPath": targetSVG,
+  "title": "Belgrade, Art Academy",
+  "latitude": 44.8048,
+  "longitude": 20.89,
+  "scale": 0.5
+}, {
+  "svgPath": targetSVG,
+  "title": "Paris, Art Academy",
+  "latitude": 48.8567,
+  "longitude": 2.7510,
+  "scale": 0.5
+}, {
+  "svgPath": targetSVG,
+  "title": "Stockholm, Art Academy",
+  "latitude": 59.3328,
+  "longitude": 17.5445,
+  "scale": 0.5
+}, {
+  "svgPath": targetSVG,
+  "title": "Prague, Art Academy",
+  "latitude": 50.0878,
+  "longitude": 14.8205,
+  "scale": 0.5
+}, {
+  "svgPath": targetSVG,
+  "title": "Buenos Aires, Art Academy",
+  "latitude": -34.6037,
+  "longitude": -58.7816,
+  "scale": 0.5
+}, {
+  "svgPath": targetSVG,
+  "title": "Madrid, Art Academy",
+  "latitude": 40.4167,
+  "longitude": -3.3033,
+  "scale": 0.5
+}];
+
+imageTemplate4.propertyFields.latitude = "latitude";
+imageTemplate4.propertyFields.longitude = "longitude";
+imageSeries4.data = [ {
+  "svgPath": targetSVG,
+  "title": "Moscow",
+  "latitude": 55.7558,
+  "longitude": 37.6176,
+  "scale": 0.5
+}, {
+  "svgPath": targetSVG,
+  "title": "Madrid",
+  "latitude": 40.4167,
+  "longitude": -3.7033,
+  "scale": 0.5
 }, {
   "svgPath": targetSVG,
   "title": "Brussels",
   "latitude": 50.8371,
   "longitude": 4.3676,
   "scale": 0.5
+}, {
+  "svgPath": targetSVG,
+  "title": "Ljubljana",
+  "latitude": 46.0514,
+  "longitude": 14.5060,
+  "scale": 0.5
+}, {
+  "svgPath": targetSVG,
+  "title": "Oslo",
+  "latitude": 59.9138,
+  "longitude": 10.7387,
+  "scale": 0.5
+} ];
+
+imageTemplate2.propertyFields.latitude = "latitude";
+imageTemplate2.propertyFields.longitude = "longitude";
+imageSeries2.data = [ {
+  "id": "london",
+  "svgPath": targetSVG,
+  "title": "London",
+  "latitude": 51.5002,
+  "longitude": -0.1262,
+  "scale": 1
 }, {
   "svgPath": targetSVG,
   "title": "Prague",
@@ -85,57 +229,15 @@ imageSeries.data = [ {
   "scale": 0.5
 }, {
   "svgPath": targetSVG,
-  "title": "Reykjavik",
-  "latitude": 64.1353,
-  "longitude": -21.8952,
-  "scale": 0.5
-}, {
-  "svgPath": targetSVG,
-  "title": "Dublin",
-  "latitude": 53.3441,
-  "longitude": -6.2675,
-  "scale": 0.5
-}, {
-  "svgPath": targetSVG,
-  "title": "Oslo",
-  "latitude": 59.9138,
-  "longitude": 10.7387,
-  "scale": 0.5
-}, {
-  "svgPath": targetSVG,
   "title": "Lisbon",
   "latitude": 38.7072,
   "longitude": -9.1355,
   "scale": 0.5
 }, {
   "svgPath": targetSVG,
-  "title": "Moscow",
-  "latitude": 55.7558,
-  "longitude": 37.6176,
-  "scale": 0.5
-}, {
-  "svgPath": targetSVG,
   "title": "Belgrade",
   "latitude": 44.8048,
   "longitude": 20.4781,
-  "scale": 0.5
-}, {
-  "svgPath": targetSVG,
-  "title": "Bratislava",
-  "latitude": 48.2116,
-  "longitude": 17.1547,
-  "scale": 0.5
-}, {
-  "svgPath": targetSVG,
-  "title": "Ljubljana",
-  "latitude": 46.0514,
-  "longitude": 14.5060,
-  "scale": 0.5
-}, {
-  "svgPath": targetSVG,
-  "title": "Madrid",
-  "latitude": 40.4167,
-  "longitude": -3.7033,
   "scale": 0.5
 }, {
   "svgPath": targetSVG,
@@ -161,109 +263,91 @@ imageSeries.data = [ {
   "latitude": 48.8567,
   "longitude": 2.3510,
   "scale": 0.5
-}, {
-  "svgPath": targetSVG,
-  "title": "New York",
-  "latitude": 40.43,
-  "longitude": -74,
-  "scale": 0.5
 } ];
 
 // Add lines
-var lineSeries = chart.series.push(new am4maps.MapLineSeries());
-lineSeries.dataFields.multiGeoLine = "multiGeoLine";
 
-var lineTemplate = lineSeries.mapLines.template;
-lineTemplate.nonScalingStroke = true;
-lineTemplate.arrow.nonScaling = true;
-lineTemplate.arrow.width = 4;
-lineTemplate.arrow.height = 6;
-lineTemplate.stroke = interfaceColors.getFor("alternativeBackground");
-lineTemplate.fill = interfaceColors.getFor("alternativeBackground");
-lineTemplate.line.strokeOpacity = 0.4;
+var lineSeries2 = chart.series.push(new am4maps.MapLineSeries());
+lineSeries2.dataFields.multiGeoLine = "multiGeoLine";
 
-lineSeries.data = [{
+var lineTemplate2 = lineSeries2.mapLines.template;
+lineTemplate2.nonScalingStroke = true;
+lineTemplate2.arrow.nonScaling = true;
+lineTemplate2.arrow.width = 4;
+lineTemplate2.arrow.height = 5;
+lineTemplate2.fill = am4core.color("#0116FF");
+lineTemplate2.stroke = am4core.color("#0116FF")
+lineTemplate2.line.strokeOpacity = 0.8;
+
+lineSeries2.data = [{
   "multiGeoLine": [
     [
-      { "latitude": 51.5002, "longitude": -0.1262 },
-      { "latitude": 50.4422, "longitude": 30.5367 }
-    ]
-  ]
-}, {
-  "multiGeoLine": [
-    [
-      { "latitude": 51.5002, "longitude": -0.1262 },
-      { "latitude": 40.4300, "longitude": -74.0000 }
-    ]
-  ]
-}, {
-  "multiGeoLine": [
-    [
-      { "latitude": 51.5002, "longitude": -0.1262 },
-      { "latitude": 64.1353, "longitude": -21.8952 }
-    ]
-  ]
-}, {
-  "multiGeoLine": [
-    [
-      { "latitude": 51.5002, "longitude": -0.1262 },
-      { "latitude": 37.9792, "longitude": 23.7166 }
-    ]
-  ]
-}, {
-  "multiGeoLine": [
-    [
-      { "latitude": 51.5002, "longitude": -0.1262 },
+      { "latitude": -34.6037, "longitude": -58.3816 },
       { "latitude": 38.7072, "longitude": -9.1355 }
     ]
   ]
-}, {
+},{
   "multiGeoLine": [
     [
-      { "latitude": 51.5002, "longitude": -0.1262 },
-      { "latitude": 55.7558, "longitude": 37.6176 }
+      { "latitude": -34.6037, "longitude": -58.3816 },
+      { "latitude": -34.6037, "longitude": -58.7816 }
     ]
   ]
 }, {
   "multiGeoLine": [
     [
-      { "latitude": 51.5002, "longitude": -0.1262 },
-      { "latitude": 44.8048, "longitude": 20.4781 }
-    ]
-  ]
-}, {
-  "multiGeoLine": [
-    [
-      { "latitude": 51.5002, "longitude": -0.1262 },
-      { "latitude": 48.2116, "longitude": 17.1547 }
-    ]
-  ]
-}, {
-  "multiGeoLine": [
-    [
-      { "latitude": 51.5002, "longitude": -0.1262 },
-      { "latitude": 46.0514, "longitude": 14.5060 }
-    ]
-  ]
-}, {
-  "multiGeoLine": [
-    [
-      { "latitude": 51.5002, "longitude": -0.1262 },
+      { "latitude": -12.0464, "longitude": -77.0428 },
       { "latitude": 40.4167, "longitude": -3.7033 }
     ]
   ]
 }, {
   "multiGeoLine": [
     [
-      { "latitude": 51.5002, "longitude": -0.1262 },
-      { "latitude": 59.3328, "longitude": 18.0645 }
+      { "latitude": 40.4167, "longitude": -3.7033 },
+      { "latitude": 40.4167, "longitude": -3.4033 }
     ]
   ]
 }, {
   "multiGeoLine": [
     [
+      { "latitude": -15.8267, "longitude": -47.9218 },
+      { "latitude": 37.9792, "longitude": 23.7166 }
+    ]
+  ]
+}, {
+  "multiGeoLine": [
+    [
+      { "latitude": 37.9792, "longitude": 23.7166 },
+      { "latitude": 44.8048, "longitude": 20.4781 }
+    ]
+  ]
+}, {
+  "multiGeoLine": [
+    [
+      { "latitude": 44.8048, "longitude": 20.4781 },
+      { "latitude": 44.8048, "longitude": 20.89 }
+    ]
+  ]
+}, {
+  "multiGeoLine": [
+    [
+      { "latitude": 44.8048, "longitude": 20.4781 },
+      { "latitude": 50.4422, "longitude": 30.5367 }
+    ]
+  ]
+}, {
+  "multiGeoLine": [
+    [
+      { "latitude": 50.4422, "longitude": 30.5367 },
+      { "latitude": 55.7558, "longitude": 37.6176 },
+      {"title": "Athens"},
+    ]
+  ]
+}, {
+  "multiGeoLine": [
+    [
+      { "latitude": 38.7072, "longitude": -9.1355 },
       { "latitude": 51.5002, "longitude": -0.1262 },
-      { "latitude": 46.9480, "longitude": 7.4481 }
     ]
   ]
 }, {
@@ -276,8 +360,92 @@ lineSeries.data = [{
 }, {
   "multiGeoLine": [
     [
-      { "latitude": 51.5002, "longitude": -0.1262 },
-      { "latitude": 19.4326, "longitude": -99.1332 }
+      { "latitude": 48.8567, "longitude": 2.3510 },
+      { "latitude": 48.8567, "longitude": 2.7510 }
+    ]
+  ]
+}, {
+  "multiGeoLine": [
+    [
+      { "latitude": 48.8567, "longitude": 2.3510 },
+      { "latitude": 50.8371, "longitude": 4.3676 }
+    ]
+  ]
+}];
+
+var lineSeries3 = chart.series.push(new am4maps.MapLineSeries());
+lineSeries3.dataFields.multiGeoLine = "multiGeoLine";
+
+var lineTemplate3 = lineSeries3.mapLines.template;
+lineTemplate3.nonScalingStroke = true;
+lineTemplate3.arrow.nonScaling = true;
+lineTemplate3.arrow.width = 4;
+lineTemplate3.arrow.height = 5;
+lineTemplate3.fill = am4core.color("#000000");
+lineTemplate3.stroke = am4core.color("#000000")
+lineTemplate3.line.strokeOpacity = 0.8;
+
+lineSeries3.data = [{
+  "multiGeoLine": [
+    [
+      { "latitude": -33.9249, "longitude": 18.4241 },
+      { "latitude": 46.9480, "longitude": 7.4481 }
+    ]
+  ]
+}, {
+  "multiGeoLine": [
+    [
+      { "latitude": 46.9480, "longitude": 7.4481 },
+      { "latitude": 50.0878, "longitude": 14.4205 }
+    ]
+  ]
+}, {
+  "multiGeoLine": [
+    [
+      { "latitude": 50.0878, "longitude": 14.4205 },
+      { "latitude": 50.0878, "longitude": 14.8205 }
+    ]
+  ]
+}, {
+  "multiGeoLine": [
+    [
+      { "latitude": 50.0878, "longitude": 14.4205 },
+      { "latitude": 46.0514, "longitude": 14.5060 }
+    ]
+  ]
+}];
+
+var lineSeries4 = chart.series.push(new am4maps.MapLineSeries());
+lineSeries4.dataFields.multiGeoLine = "multiGeoLine";
+
+var lineTemplate4 = lineSeries4.mapLines.template;
+lineTemplate4.nonScalingStroke = true;
+lineTemplate4.arrow.nonScaling = true;
+lineTemplate4.arrow.width = 4;
+lineTemplate4.arrow.height = 5;
+lineTemplate4.fill = am4core.color("#e03e96");
+lineTemplate4.stroke = am4core.color("#e03e96")
+lineTemplate4.line.strokeOpacity = 0.8;
+
+lineSeries4.data = [{
+  "multiGeoLine": [
+    [
+      { "latitude": 37.5665, "longitude": 126.9780 },
+      { "latitude": 59.3328, "longitude": 18.0645 }
+    ]
+  ]
+}, {
+  "multiGeoLine": [
+    [
+      { "latitude": 59.3328, "longitude": 18.0645 },
+      { "latitude": 59.9138, "longitude": 10.7387 }
+    ]
+  ]
+}, {
+  "multiGeoLine": [
+    [
+      { "latitude": 59.3328, "longitude": 18.0645 },
+      { "latitude": 59.3328, "longitude": 17.5445 }
     ]
   ]
 }];
